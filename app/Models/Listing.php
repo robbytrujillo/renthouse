ab6c3a1547709366d45317ba2fc453149d526b19
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-18 06:23:34 +0700

    introduction renthouse | listing Resource

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
 
 class Listing extends Model
 {
@@ -16,6 +17,26 @@ class Listing extends Model
         'description',
         'address',
         'sqft',
-        
+        'wifi_speed',
+        'max_person',
+        'price_per_day',
+        'attachments',
+        'full_support_available',
+        'gym_area_available',
+        'mini_cafe_available',
+        'cinema_available'
     ];
+
+    protected $cast = [
+        'attachments' => 'array'
+    ];
+
+    public function getRouteKeyName() {
+        return 'slug';
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
