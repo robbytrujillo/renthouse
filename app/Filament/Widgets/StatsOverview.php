4cdbbaca915247f1a899c61d5b0a1be88fd4559a
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-02 05:46:46 +0700

    introduction renthouse | Dasboard Status Overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,15 +2,29 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Listing;
+use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
+    private function getPercentage(int $from,int $to) {
+        return $to - $from / ($to + $from / 2) * 100;
+    }
     protected function getStats(): array
     {
+        $newListing = Listing::whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)->count();
+        $transactions = Transaction::whereStatus('approved')->whereMonth('created_at',Carbon::now()->month)->whereYear('created_at', Carbon::now()->year);
+        $prevTransactions = Transaction::whereStatus('approved')->whereMonth('created_at',Carbon::now()->subMonth()->month)->whereYear('created_at', Carbon::now()->subMonth()->year);
+        $transactionPercentage = $this->getPercentage($prevTransactions->count(), $transactions->count());
+
         return [
-            //
+            Stat::make('New listing of the month', $newListing),
+            Stat::make('Transaction of the month', $transactions->count())
+                ->description($transactionPercentage > 0 ? "{$transactionPercentage}% increased" : "{$transactionPercentage}% decreased"),
+            // Stat::make('Average time on page', '3:12'),
         ];
     }
 }
