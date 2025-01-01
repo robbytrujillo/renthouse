6fb0e48af04e1ff1c9f4f29f9a586d441b0af99e
robbytrujillo <robbyikusuma88@gmail.com>
2025-01-02 06:04:48 +0700

    introduction renthouse | Dasboard Status Overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -7,6 +7,7 @@ use App\Models\Transaction;
 use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
@@ -19,11 +20,18 @@ class StatsOverview extends BaseWidget
         $transactions = Transaction::whereStatus('approved')->whereMonth('created_at',Carbon::now()->month)->whereYear('created_at', Carbon::now()->year);
         $prevTransactions = Transaction::whereStatus('approved')->whereMonth('created_at',Carbon::now()->subMonth()->month)->whereYear('created_at', Carbon::now()->subMonth()->year);
         $transactionPercentage = $this->getPercentage($prevTransactions->count(), $transactions->count());
+        $revenuePercentage = $this->getPercentage($prevTransactions->sum('total_price'), $transactions->sum('total_price'));
 
         return [
             Stat::make('New listing of the month', $newListing),
             Stat::make('Transaction of the month', $transactions->count())
-                ->description($transactionPercentage > 0 ? "{$transactionPercentage}% increased" : "{$transactionPercentage}% decreased"),
+                ->description($transactionPercentage > 0 ? "{$transactionPercentage}% increased" : "{$transactionPercentage}% decreased")
+                ->descriptionIcon($transactionPercentage > 0 ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down')
+                ->color($transactionPercentage > 0 ? 'success' : 'danger'),
+            Stat::make('Revenue of the month', Number::currency($transactions->sum('total_price'), 'USD'))
+                ->description($revenuePercentage > 0 ? "{$revenuePercentage}% increased" : "{$revenuePercentage}% decreased")
+                ->descriptionIcon($revenuePercentage > 0 ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down')
+                ->color($revenuePercentage > 0 ? 'success' : 'danger'),
             // Stat::make('Average time on page', '3:12'),
         ];
     }
